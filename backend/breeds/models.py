aa2302dba6a1d3a8d226e85b54d45b9d385bfce4
Kirill <bku089@gmail.com>
2026-03-20 18:21:04 +0200

    Add Favorite model for user-breed relation

--- backend/breeds/models.py
+++ backend/breeds/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -53,3 +54,11 @@ class Breed(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("user", "breed")
