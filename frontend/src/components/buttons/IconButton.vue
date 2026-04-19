2648df7a69e2521fbfed79a22c35ef6b16391c6d
Yaroslav <yaroslavhohulia@gmail.com>
2026-04-19 13:30:04 +0300

    added component for icon button

---
+++ frontend/src/components/buttons/IconButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+  defineProps({
+    icon: {
+      type: Object,
+      required: true,
+    },
+  });
+</script>
+
+<template>
+  <button class="btn btn-icon" v-bind="$attrs">
+    <component :is="icon" class="size-6 block" />
+  </button>
+</template>
