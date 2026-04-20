cdeff60f122a70a687985d2955d24be89e4077e3
Yaroslav <yaroslavhohulia@gmail.com>
2026-04-20 11:23:19 +0300

    deleted comments

--- frontend/src/components/assets/AppCheckbox.vue
+++ frontend/src/components/assets/AppCheckbox.vue
@@ -1,24 +1,3 @@
-<!-- <script setup>
-defineOptions({ inheritAttrs: false });
-
-defineProps({
-  icon: {
-    type: Object,
-    required: true,
-  },
-});
-</script>
-
-<template>
-  <label class="flex items-center gap-2 cursor-pointer">
-    <input type="checkbox" class="form-input-hidden" v-bind="$attrs" />
-
-    <div class="checkbox">
-      <component :is="icon" class="w-4 h-4" />
-    </div>
-  </label>
-</template> -->
-
 <script setup>
 defineOptions({ inheritAttrs: false });
 
