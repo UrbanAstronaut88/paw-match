b532429e783a69fcec8bc645d9735dd3cc3f3fde
Yaroslav <yaroslavhohulia@gmail.com>
2026-04-26 14:34:20 +0300

    added AppLayout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,8 @@
 <script setup>
-import Footer from "./components/Footer.vue";
-import Header from "./components/Header.vue";
-import Test from "./components/Test.vue";
+import AuthContainer from "./components/AuthContainer.vue";
+import AppLayout from "./components/AppLayout.vue";
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen">
-    <Header />
-
-    <main class="flex-1 max-w-[1296px] w-full mx-auto">
-      <Test />
-    </main>
-
-    <Footer />
-  </div>
+  <AuthContainer />
 </template>
